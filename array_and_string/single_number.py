6ba9174eac442be45a5219b3cfac1ca96c95aede
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2023-09-23 21:38:34 -0400

    array

---
+++ array_and_string/single_number.py
@@ -0,0 +1,14 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        single = 0
+
+        for num in nums:
+            single ^= num
+        return single
